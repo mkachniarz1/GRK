c07198a9c19e4a8cce1fac05b524affd38158b0f
Maciej Kachniarz <maciej.kachniarz@predica.pl>
2018-10-08 14:44:14 +0200

    Set all pixels to magenta color

--- grk_cw1/na 3/sketch.js
+++ grk_cw1/na 3/sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-
+    noLoop();
 }
 
 function draw() {
-
+    //noprotect
+    for (y = 0; y < height; y++)
+        for (x = 0; x < width; x++) {
+            set(x, y, color(255, 0, 144));
+            updatePixels();
+        }
+    
 }
