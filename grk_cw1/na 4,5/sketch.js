211d1bbb58a7870b1c77d0e85a60730d0603e54b
Maciej Kachniarz <maciej.kachniarz@predica.pl>
2018-10-08 15:51:14 +0200

    set sky na 4,5

---
+++ grk_cw1/na 4,5/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    noLoop();
+    createCanvas(800, 600);
+}
+
+function draw() {
+    //noprotect
+    for (y = 0; y < height; y++)
+        for (x = 0; x < width; x++) {
+            // niebo
+            set(x, y, color(200, 200, 255))
+
+            updatePixels();
+        }
+}
