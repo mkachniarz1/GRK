cf68434dd68540a9f71dcf700db01a9e2914900b
Maciej Kachniarz <s15770@pjwstk.edu.pl>
2018-10-11 09:24:19 +0200

    init env na 4

---
+++ grk_cw2/na 4/sketch.js
@@ -0,0 +1,47 @@
+function preload() {
+    img = loadImage("https://raw.githubusercontent.com/scikit-image/scikit-image/master/skimage/data/astronaut.png");
+    img_h = createImage(256, 256);
+    img_s = createImage(256, 256);
+    img_v = createImage(256, 256);
+}
+
+function setup() {
+    createCanvas(512, 512);
+    img.resize(256, 256);
+}
+
+function draw() {
+    noLoop();
+    img.loadPixels();
+    img_h.loadPixels();
+    img_s.loadPixels();
+    img_v.loadPixels();
+
+    
+
+    d = pixelDensity();
+    for (x = 0; x < img.width; x++)
+        for (y = 0; y < img.height; y++)
+            for (dx = 0; dx < d; dx++)
+                for (dy = 0; dy < d; dy++) {
+                    pos = 4 * ((y * d + dy) * img.width * d + (x * d + dx));
+                    r = img.pixels[pos] / 255;
+                    g = img.pixels[pos + 1] / 255;
+                    b = img.pixels[pos + 2] / 255;
+                    cmax = Math.max(r, g, b);
+                    cmin = Math.min(r, g, b);
+                    l = (cmax + cmin) / 2;
+                    v = cmax;
+
+                    img_v.set(x, y, 255 * v);
+                    img_h.set(x, y, 255 * l);
+                }
+    img_h.updatePixels();
+    img_s.updatePixels();
+    img_v.updatePixels();
+
+    image(img_h, 0, 0);
+    image(img_s, 255, 0);
+    image(img_v, 0, 255);
+    image(img, 255, 255);
+}
