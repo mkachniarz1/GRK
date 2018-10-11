d269de4a21f3fdb0eb66ce9fa9b10d32e8b913b3
Maciej Kachniarz <s15770@pjwstk.edu.pl>
2018-10-11 09:35:16 +0200

    add hue

---
+++ grk_cw2/na 4,5/sketch.js
@@ -0,0 +1,64 @@
+function preload() {
+    img = loadImage("https://raw.githubusercontent.com/scikit-image/scikit-image/master/skimage/data/astronaut.png");
+    img_h = createImage(256, 256);
+    img_s = createImage(256, 256);
+    img_v = createImage(256, 256);
+}
+
+function setup() {
+    createCanvas(512, 512);
+    img.resize(256, 256);
+}
+
+function draw() {
+    noLoop();
+    img.loadPixels();
+    img_h.loadPixels();
+    img_s.loadPixels();
+    img_v.loadPixels();
+
+
+
+    d = pixelDensity();
+    for (x = 0; x < img.width; x++)
+        for (y = 0; y < img.height; y++)
+            for (dx = 0; dx < d; dx++)
+                for (dy = 0; dy < d; dy++) {
+                    pos = 4 * ((y * d + dy) * img.width * d + (x * d + dx));
+                    r = img.pixels[pos] / 255;
+                    g = img.pixels[pos + 1] / 255;
+                    b = img.pixels[pos + 2] / 255;
+                    cmax = Math.max(r, g, b);
+                    cmin = Math.min(r, g, b);
+                    c = cmax - cmin;
+                    l = (cmax + cmin) / 2;
+                    v = cmax;
+                    s = cmax == 0 ? 0 : c / cmax;
+
+                    if (c == 0)
+                        h = 0;
+                    else if (v == r)
+                        h = ((g - b) / c) % 6;
+                    else if (v == g)
+                        h = ((b - r) / c) + 2;
+                    else /*v==b*/
+                        h = ((r - g) / c) + 4;
+
+                    h /= 6;
+                    if(h<0) h+=1;
+
+
+                    img_v.set(x, y, 255 * v);
+                    img_h.set(x, y, 255 * h);
+                    img_s.set(x, y, 255 * s);
+
+                }
+    img_h.updatePixels();
+    img_s.updatePixels();
+    img_v.updatePixels();
+
+    image(img_h, 0, 0);
+    image(img_s, 255, 0);
+    image(img_v, 0, 255);
+    image(img, 255, 255);
+}
