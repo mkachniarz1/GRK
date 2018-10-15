2bd73953eebccc2f9489368516cd6ab580a03d4a
Maciek <mkachniarz1@gmail.com>
2018-10-15 19:33:21 +0200

    fill sketch from excercise

--- grk_cw3/na 3/sketch.js
+++ grk_cw3/na 3/sketch.js
@@ -1,8 +1,32 @@
-function setup(){
+function setup() {
     createCanvas(512, 521);
     background(255);
 }
-var x0=-1;
-var y0=-1;
-var x1=-1;
-var y1=-1;
+
+var x0 = -1;
+var y0 = -1;
+var x1 = -1;
+var y1 = -1;
+
+function mousePressed() {
+    x0 = mouseX;
+    y0 = mouseY;
+}
+
+function mouseDragged() {
+    x1 = mouseX;
+    y1 = mouseY;
+    background(255);
+    noStroke();
+    fill('red');
+    ellipse(x0 - 3, y0 - 3, 6);
+    fill('green');
+    ellipse(x1 - 3, y1 - 3, 6);
+}
+
+function mouseReleased() {
+    background(255);
+    loadPixels();
+    draw_line();
+    updatePixels();
+  }
