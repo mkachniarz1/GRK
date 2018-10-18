79066771da82e757aaec8ea8a7bfc9f0fc626338
Maciej Kachniarz <s15770@pjwstk.edu.pl>
2018-10-18 09:09:05 +0200

    fix na 5

--- grk_cw2/na 5/sketch.js
+++ grk_cw2/na 5/sketch.js
@@ -1,6 +1,6 @@
 function preload() {
     img = loadImage("https://raw.githubusercontent.com/scikit-image/scikit-image/master/skimage/data/astronaut.png");
-    img.filter('gray')
+    
 }
 
 function setup() {
@@ -8,8 +8,10 @@ function setup() {
 }
 
 function draw() {
+    //noprotect
     noLoop();
-        var histogram = new Array(256);
+    img.filter('gray')
+    var histogram = new Array(256);
     histogram.fill(0);
 
     img.loadPixels();
@@ -17,16 +19,17 @@ function draw() {
         for (y = 0; y < img.height; y++) {
             pos = 4 * (y * img.width + x);
             histogram[img.pixels[pos]] += 1;
-
         }
 
-    console.log(histogram);
-
-
+    background(255);
     stroke(0);
-    for (var i = 0; i < histogram.lenght; i++) {
-        l = histogram[i]/histogram.lenght*height;
-        line(i, 255, i,  l);
+
+    for (i = 0; i < histogram.length ; i++){
+        var l = histogram[i]/Math.max(...histogram)*height;
+        console.log(l);
+
+        line(i, 255, i, height - l);
     }
+
 
 }
